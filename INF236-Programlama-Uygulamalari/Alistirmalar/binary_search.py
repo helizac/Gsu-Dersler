5279191cb4bc6541d2ddb1a74f59db0262f2ed78
Furkan Erdi <54884571+helizac@users.noreply.github.com>
2022-01-07 23:38:24 +0300

    Add files via upload

---
+++ INF236-Programlama-Uygulamalari/Alistirmalar/binary_search.py
@@ -0,0 +1,73 @@
+def recursive_binary_search(_array, low, high, x):
+    # Check base case
+    if high >= low:
+
+        mid = (high + low) // 2
+
+        # If element is present at the middle itself
+        if arr[mid] == x:
+            return mid
+
+        # If element is smaller than mid, then it can only
+        # be present in left subarray
+        elif arr[mid] > x:
+            return recursive_binary_search(arr, low, mid - 1, x)
+
+        # Else the element can only be present in right subarray
+        else:
+            return recursive_binary_search(arr, mid + 1, high, x)
+
+    else:
+        # Element is not present in the array
+        return -1
+
+
+# Test array
+arr = [2, 3, 4, 10, 40]
+x = 10
+
+# Function call
+result = recursive_binary_search(arr, 0, len(arr) - 1, x)
+
+if result != -1:
+    print("Element is present at index", str(result))
+else:
+    print("Element is not present in array")
+
+
+def iterative_binary_search(_array, x):
+    low = 0
+    high = len(_array) - 1
+    mid = 0
+
+    while low <= high:
+
+        mid = (high + low) // 2
+
+        # If x is greater, ignore left half
+        if _array[mid] < x:
+            low = mid + 1
+
+        # If x is smaller, ignore right half
+        elif _array[mid] > x:
+            high = mid - 1
+
+        # means x is present at mid
+        else:
+            return mid
+
+    # If we reach here, then the element was not present
+    return -1
+
+
+# Test array
+array1 = [2, 3, 4, 10, 40]
+x = 10
+
+# Function call
+result = iterative_binary_search(array1, x)
+
+if result != -1:
+    print("Element is present at index", str(result))
+else:
+    print("Element is not present in array")
