f2ad012eb022caaca09ff9f60987e36f00bd77b5
Furkan Erdi <54884571+helizac@users.noreply.github.com>
2022-02-28 21:17:24 +0300

    Delete INF223-Object-Oriented-Programming/class directory

--- INF223-Object-Oriented-Programming/class/class1/src/automobile.java
+++
@@ -1,9 +0,0 @@
-class automobile {
-    private String brand; // property
-    public void Automobile(String brand){
-        this.setBrand(brand);
-    }
-
-    public String getBrand(){return this.brand;}
-    public void setBrand(String brand){this.brand = brand;}
-}
